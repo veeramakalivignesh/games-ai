75132f5822974e52f8ba1cd9119d78e785081450
M Veeramakali Vignesh <veeramakali1@gmail.com>
2024-07-21 19:21:03 -0700

    cleaned deployment scripts

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: ./setup.sh && uvicorn src.python.app:app --host 0.0.0.0 --port $PORT
--- src/python/app.py
+++ src/python/app.py
@@ -5,10 +5,7 @@ from src.python.cannon_bot_service import CannonBotService
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://ec2-3-92-59-237.compute-1.amazonaws.com:3000", 
-                   "http://games-ai-1184075193.us-east-1.elb.amazonaws.com", 
-                   "http://play.gamesai.co.in",
-                   "https://games-ai-ui-abfe0e7a7e95.herokuapp.com",
+    allow_origins=["https://games-ai-ui-abfe0e7a7e95.herokuapp.com",
                    "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
