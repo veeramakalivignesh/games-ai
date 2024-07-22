42f08b38a127b42036db4e982908ed058f9989cd
M Veeramakali Vignesh <veeramakali1@gmail.com>
2024-07-21 18:19:29 -0700

    changed setup

--- setup.sh
+++ setup.sh
@@ -6,10 +6,6 @@ g++ -c -fPIC -std=c++11 -O3 -o ./bin/bot.o src/cpp/bot/src/Bot.cpp
 g++ -c -fPIC -std=c++11 -O3 -o ./bin/abstract_bot.o src/cpp/bot/src/AbstractBot.cpp
 g++ -c -fPIC -std=c++11 -O3 -o ./bin/cannon_bot.o src/cpp/bot/src/cannon/CannonBot.cpp
 
-if [[ $OSTYPE == 'darwin'* ]]; then
-    g++ -shared -Wl,-install_name,./bin/botlib.so -o ./bin/botlib.so ./bin/bot.o ./bin/abstract_bot.o ./bin/cannon_bot.o
-else
-    g++ -shared -Wl,-soname,./bin/botlib.so -o ./bin/botlib.so ./bin/bot.o ./bin/abstract_bot.o ./bin/cannon_bot.o
-fi
+g++ -shared -Wl,-soname,./bin/botlib.so -o ./bin/botlib.so ./bin/bot.o ./bin/abstract_bot.o ./bin/cannon_bot.o
 
 echo "Setup complete"
