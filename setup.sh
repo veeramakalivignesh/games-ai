807f1704fd720f27bc9fbb0ae97e9f5f5b24e562
M Veeramakali Vignesh <veeramakali1@gmail.com>
2024-07-21 18:16:08 -0700

    changed setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 echo "Creating bin directory"
 mkdir -p bin
 
@@ -8,7 +6,7 @@ g++ -c -fPIC -std=c++11 -O3 -o ./bin/bot.o src/cpp/bot/src/Bot.cpp
 g++ -c -fPIC -std=c++11 -O3 -o ./bin/abstract_bot.o src/cpp/bot/src/AbstractBot.cpp
 g++ -c -fPIC -std=c++11 -O3 -o ./bin/cannon_bot.o src/cpp/bot/src/cannon/CannonBot.cpp
 
-if [ "${OSTYPE:0:6}" = "darwin" ]; then
+if [[ $OSTYPE == 'darwin'* ]]; then
     g++ -shared -Wl,-install_name,./bin/botlib.so -o ./bin/botlib.so ./bin/bot.o ./bin/abstract_bot.o ./bin/cannon_bot.o
 else
     g++ -shared -Wl,-soname,./bin/botlib.so -o ./bin/botlib.so ./bin/bot.o ./bin/abstract_bot.o ./bin/cannon_bot.o
